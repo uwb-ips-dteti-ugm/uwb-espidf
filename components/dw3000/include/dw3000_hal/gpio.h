bac57845e9f8c6227f1e97c4910e00845609d263
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2026-04-30 15:25:57 +0700

    feat/hal-api/codex: (chore) Unused stdint removed.

--- components/dw3000/include/dw3000_hal/gpio.h
+++ components/dw3000/include/dw3000_hal/gpio.h
@@ -2,7 +2,6 @@
 #define DW3000_HAL_GPIO_H
 
 #include <stdbool.h>
-#include <stdint.h>
 
 #include "dw3000_device.h"
 #include "dw3000_error.h"
@@ -41,96 +40,96 @@ dw3000_error_t dw3000_hal_gpio_disable_clocks(
 );
 
 dw3000_error_t dw3000_hal_gpio_read_mode(
-    dw3000_device_t*      device,
-    dw3000_gpio_mode_t*   mode
+    dw3000_device_t*    device,
+    dw3000_gpio_mode_t* mode
 );
 
 dw3000_error_t dw3000_hal_gpio_configure_mode(
-    dw3000_device_t*            device,
-    const dw3000_gpio_mode_t*   mode
+    dw3000_device_t*          device,
+    const dw3000_gpio_mode_t* mode
 );
 
 dw3000_error_t dw3000_hal_gpio_set_pin_function(
-    dw3000_device_t*           device,
-    dw3000_gpio_pin_index_t    pin,
-    dw3000_gpio_function_t     function
+    dw3000_device_t*        device,
+    dw3000_gpio_pin_index_t pin,
+    dw3000_gpio_function_t  function
 );
 
 dw3000_error_t dw3000_hal_gpio_read_pull_enable(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t*  pins
+    dw3000_device_t*   device,
+    dw3000_gpio_pin_t* pins
 );
 
 dw3000_error_t dw3000_hal_gpio_set_pull_enable(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t   pins
+    dw3000_device_t*  device,
+    dw3000_gpio_pin_t pins
 );
 
 dw3000_error_t dw3000_hal_gpio_read_direction(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t*  input_pins
+    dw3000_device_t*   device,
+    dw3000_gpio_pin_t* input_pins
 );
 
 dw3000_error_t dw3000_hal_gpio_set_direction(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t   input_pins
+    dw3000_device_t*  device,
+    dw3000_gpio_pin_t input_pins
 );
 
 dw3000_error_t dw3000_hal_gpio_read_output(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t*  output_pins
+    dw3000_device_t*   device,
+    dw3000_gpio_pin_t* output_pins
 );
 
 dw3000_error_t dw3000_hal_gpio_set_output(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t   output_pins
+    dw3000_device_t*  device,
+    dw3000_gpio_pin_t output_pins
 );
 
 dw3000_error_t dw3000_hal_gpio_set_pins(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t   pins
+    dw3000_device_t*  device,
+    dw3000_gpio_pin_t pins
 );
 
 dw3000_error_t dw3000_hal_gpio_clear_pins(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t   pins
+    dw3000_device_t*  device,
+    dw3000_gpio_pin_t pins
 );
 
 dw3000_error_t dw3000_hal_gpio_read_raw(
-    dw3000_device_t*    device,
-    dw3000_gpio_raw_t*  raw
+    dw3000_device_t*   device,
+    dw3000_gpio_raw_t* raw
 );
 
 dw3000_error_t dw3000_hal_gpio_read_irq_status(
-    dw3000_device_t*              device,
-    dw3000_gpio_irq_status_t*     status
+    dw3000_device_t*          device,
+    dw3000_gpio_irq_status_t* status
 );
 
 dw3000_error_t dw3000_hal_gpio_clear_irq_status(
-    dw3000_device_t*             device,
-    dw3000_gpio_irq_status_t     status
+    dw3000_device_t*         device,
+    dw3000_gpio_irq_status_t status
 );
 
 dw3000_error_t dw3000_hal_gpio_read_irq_cfg(
-    dw3000_device_t*         device,
-    dw3000_gpio_irq_cfg_t*   irq
+    dw3000_device_t*       device,
+    dw3000_gpio_irq_cfg_t* irq
 );
 
 dw3000_error_t dw3000_hal_gpio_configure_irq(
-    dw3000_device_t*               device,
-    const dw3000_gpio_irq_cfg_t*    irq
+    dw3000_device_t*             device,
+    const dw3000_gpio_irq_cfg_t* irq
 );
 
 dw3000_error_t dw3000_hal_gpio_configure(
-    dw3000_device_t*              device,
-    const dw3000_gpio_config_t*   config
+    dw3000_device_t*            device,
+    const dw3000_gpio_config_t* config
 );
 
 dw3000_error_t dw3000_hal_gpio_configure_current(dw3000_device_t* device);
 
 dw3000_error_t dw3000_hal_gpio_configure_led_functions(
-    dw3000_device_t*    device,
-    dw3000_gpio_pin_t   led_pins
+    dw3000_device_t*  device,
+    dw3000_gpio_pin_t led_pins
 );
 
 dw3000_error_t dw3000_hal_gpio_configure_external_pa_lna(
